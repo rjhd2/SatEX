60ffc39be8c6034668bf0ab7d4235e9d5116c578
Veronika P?rtge <veronika.poertge@gmail.com>
2018-08-07 16:48:27 +0100

    Started writing a script which produces LaTeX figures

--- plot_climpact_indices.py
+++ plot_climpact_indices.py
@@ -1,7 +1,7 @@
 import numpy as np
 import iris
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import iris.quickplot as qplt
 import cartopy.crs as ccrs
--- stick_tiles_of_indices_together.py
+++ stick_tiles_of_indices_together.py
@@ -1,7 +1,7 @@
 import numpy as np
 import iris
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import iris.quickplot as qplt
 #import matplotlib.cm as mpl_cm
